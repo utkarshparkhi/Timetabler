707c6cc0b808b9997f735da603d9f215bae907e0
utkarsh parkhi <42688747+utkarshparkhi@users.noreply.github.com>
2020-12-19 22:06:25 +0530

    initial ideas

---
+++ course.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 19 10:00:23 2020
+
+@author: parkh
+"""
+
+class Course:
+    def __init__(self):
+        self.lectures = 0
+        self.size = 0
+        self.faculty = []
+        self.timing = ''
+        
---
+++ untitled0.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 19 22:04:05 2020
+
+@author: parkh
+"""
+
+class Room:
+    def __int__(self):
+        
